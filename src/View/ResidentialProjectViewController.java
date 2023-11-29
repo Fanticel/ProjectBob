bcb7a10f15c3d519c0b268aa230e5b5fd8679208
AKStifter <95703322+AKStifter@users.noreply.github.com>
2023-11-29 11:55:31 +0100

    Controller for residentialProjectView
    Created empty controller for residentailProjectView

---
+++ src/View/ResidentialProjectViewController.java
@@ -0,0 +1,43 @@
+package View;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class ResidentialProjectViewController {
+
+  @FXML
+  private RadioButton RenovationRButton;
+
+  @FXML
+  private RadioButton buildRButton;
+
+  @FXML
+  private TextField expectedExpensesField;
+
+  @FXML
+  private TextField expectedTotalHoursField;
+
+  @FXML
+  private TextField expensesField;
+
+  @FXML
+  private ToggleGroup newBuildGroup;
+
+  @FXML
+  private TextField numBathroomsField;
+
+  @FXML
+  private TextField numKitchensField;
+
+  @FXML
+  private TextField othWPlumbingField;
+
+  @FXML
+  private TextField sizeField;
+
+  @FXML
+  private TextField totalHoursField;
+
+}
