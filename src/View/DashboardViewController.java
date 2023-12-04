257a668ce6e1635c131a5d473cf2bb663c97c264
JosipBr <josipbrljevic@gmail.com>
2023-12-04 21:53:59 +0100

    Dashboard view with project template

--- src/View/DashboardViewController.java
+++ src/View/DashboardViewController.java
@@ -1,31 +1,105 @@
 package View;
 
+import Model.Project;
+import Model.ProjectListModelManager;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import Model.ProjectListModel;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
-public class DashboardViewController {
+import java.util.ArrayList;
+import java.util.List;
+
+public class DashboardViewController
+{
   private ProjectListModel model;
   private ViewHandler viewHandler;
   private Region root;
-  public DashboardViewController(){}
-  public void init(ViewHandler viewHandler, ProjectListModel model, Region root){
+
+  @FXML private ScrollPane scrollPane;
+
+  @FXML private HBox noProjects;
+
+
+  public DashboardViewController()
+  {
+  }
+
+  public void init(ViewHandler viewHandler, ProjectListModel model, Region root)
+  {
     this.viewHandler = viewHandler;
     this.model = model;
     this.root = root;
-  }
-  public void reset(){
+
+    ProjectListModelManager projectListModelManager = new ProjectListModelManager();
+    projectListModelManager.getFromFile();
+
+    ArrayList<Project> ongoingProjects = new ArrayList<>();
+
+    for (Project i : projectListModelManager.getAllProjects()
+        .returnAsArrayList())
+    {
+      if (i.getStatus().equals("Ongoing"))
+      {
+        ongoingProjects.add(i);
+      }
+    }
+
+    VBox container = new VBox();
+
+    if(ongoingProjects.isEmpty()){
+      noProjects.setVisible(true);
+    }
+    else {
+    for (Project i : ongoingProjects)
+    {
+      try
+      {
+        FXMLLoader loader = new FXMLLoader(
+            getClass().getResource("ProjectTemplateView.fxml"));
+
+        Parent projectTemplate = loader.load();
+        ProjectTemplateViewController controller = loader.getController();
+
+        controller.setProjectInfo(i);
+        container.getChildren().add(projectTemplate);
+
+      }
+      catch (Exception e)
+      {
+        e.printStackTrace();
+      }
+    }
+    scrollPane.setContent(container);
+    noProjects.setVisible(true);
+  }}
+
+  public void reset()
+  {
 
   }
-  public Region getRoot(){
+
+  public Region getRoot()
+  {
     return root;
   }
 
-  @FXML private void clickProjectsButton(){
+  @FXML private void clickProjectsButton()
+  {
     viewHandler.openView("ProjectList");
   }
 
-  @FXML private void clickHomeButton(){
+  @FXML private void clickHomeButton()
+  {
     viewHandler.openView("");
   }
+
 }
---
+++ src/View/ProjectTemplateViewController.java
@@ -0,0 +1,75 @@
+package View;
+
+import Model.NotificationDetector;
+import Model.Project;
+import Model.ProjectListModel;
+import Model.ProjectListModelManager;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+
+public class ProjectTemplateViewController
+{
+  private ProjectListModel model;
+  private ViewHandler viewHandler;
+  private Region root;
+  private ProjectListViewModel projectListViewModel;
+
+  @FXML private Label type;
+  @FXML private TextArea description;
+  @FXML private Label deadlineNotification;
+  @FXML private Label fundsNotification;
+  @FXML private Label name;
+  @FXML private Label manHours;
+  @FXML private Label expectedPrice;
+  @FXML private Label estimatedHours;
+
+
+  public ProjectTemplateViewController(){}
+  public void init(ViewHandler viewHandler, ProjectListModel model, Region root){
+    this.viewHandler = viewHandler;
+    this.model = model;
+    this.root = root;
+  }
+
+  public void setProjectInfo(Project project) {
+    NotificationDetector notificationDetector = new NotificationDetector(project);
+
+    if (notificationDetector.checkDeadline() == null) {
+      Pane parent = (Pane) deadlineNotification.getParent();
+      parent.getChildren().remove(deadlineNotification);
+    } else {
+      deadlineNotification.setText(notificationDetector.checkDeadline());
+    }
+
+    if (notificationDetector.checkFunds() == null) {
+      Pane parent = (Pane) fundsNotification.getParent();
+      parent.getChildren().remove(fundsNotification);
+    } else {
+      fundsNotification.setText(notificationDetector.checkFunds());
+    }
+
+    name.setText(project.getName());
+    type.setText(type.getText() + " " + project.getClass().getSimpleName());
+    description.setText(project.getDescription());
+    estimatedHours.setText(estimatedHours.getText() + " " + String.valueOf(project.getExpectedTotalHours()) + " hours");
+    expectedPrice.setText(expectedPrice.getText() + " " + String.valueOf(project.getExpectedExpenses()) + " DK");
+  }
+
+
+
+  public void reset(){
+
+  }
+  public Region getRoot(){
+    return root;
+  }
+
+}
